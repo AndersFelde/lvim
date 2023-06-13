d8fcdbfc0b8f559fa9bf4cc4f6c50b025524fdb0
AndersFelde <anderskfelde@hotmail.com>
2023-06-13 22:59:42 +0200

    funker ganske bra nå egt

--- config.lua
+++ config.lua
@@ -6,5 +6,18 @@ lvim.keys.normal_mode["J"] = "5j"
 lvim.keys.normal_mode["K"] = "5k"
 lvim.keys.normal_mode["L"] = "w"
 lvim.keys.normal_mode["H"] = "b"
-
 lvim.lsp.buffer_mappings.normal_mode['K'] = "5k"
+
+lvim.keys.visual_mode["J"] = "5j"
+lvim.keys.visual_mode["K"] = "5k"
+lvim.keys.visual_mode["L"] = "w"
+lvim.keys.visual_mode["H"] = "b"
+lvim.lsp.buffer_mappings.visual_mode['K'] = "5k"
+
+lvim.keys.normal_mode["<C-w>"] = "<cmd>BufferKill<CR>"
+lvim.keys.normal_mode["<Tab>"] = "<cmd>bnext<CR>"
+lvim.keys.normal_mode["<S-Tab>"] = "<cmd>bnext<CR>"
+lvim.keys.normal_mode["<C-n>"] = "<cmd>NvimTreeToggle<CR>"
+
+lvim.builtin.which_key.vmappings["c"] = lvim.builtin.which_key.vmappings["/"]
+lvim.builtin.which_key.mappings["c"] = lvim.builtin.which_key.mappings["/"]
