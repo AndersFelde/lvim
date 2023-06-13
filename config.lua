d7a416722073ad4903a0611616ea853d841d95d7
AndersFelde <anderskfelde@hotmail.com>
2023-06-13 22:24:13 +0200

    first commit

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.keys.normal_mode["J"] = "5j"
+lvim.keys.normal_mode["K"] = "5k"
+lvim.keys.normal_mode["L"] = "w"
+lvim.keys.normal_mode["H"] = "b"
+
+lvim.lsp.buffer_mappings.normal_mode['K'] = "5k"
